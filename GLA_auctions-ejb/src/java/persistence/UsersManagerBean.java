569b4cb801fde32701df010636d3c13e12c8f971
JcLionnet <jclionnet@hotmail.fr>
2019-01-24 15:56:06 +0100

    Ajout exemple inscription pour Lucas

--- GLA_auctions-ejb/src/java/persistence/UsersManagerBean.java
+++ GLA_auctions-ejb/src/java/persistence/UsersManagerBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package persistance;
+package persistence;
 
 import static java.lang.reflect.Array.set;
 import java.sql.Connection;
--- GLA_auctions-ejb/src/java/persistence/UsersManagerBeanLocal.java
+++ GLA_auctions-ejb/src/java/persistence/UsersManagerBeanLocal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package persistance;
+package persistence;
 
 import java.sql.Date;
 import java.util.List;
