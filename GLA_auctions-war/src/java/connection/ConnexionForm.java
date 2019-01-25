6e4a28023570245bf97c90fc61632cc66e6b16a6
JcLionnet <jclionnet@hotmail.fr>
2019-01-25 11:56:44 +0100

    login test

---
+++ GLA_auctions-war/src/java/connection/ConnexionForm.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import model.Users;
+import persistence.UsersManagerBean;
+
+public final class ConnexionForm {
+    private static final String CHAMP_EMAIL  = "email";
+    private static final String CHAMP_PASS   = "motdepasse";
+
+    private String              resultat;
+    private Map<String, String> erreurs      = new HashMap<String, String>();
+    private UsersManagerBean usersManager = new UsersManagerBean();
+
+    public String getResultat() {
+        return resultat;
+    }
+
+    public Map<String, String> getErreurs() {
+        return erreurs;
+    }
+
+    public Users connecterUtilisateur( HttpServletRequest request ) {
+        /* Récupération des champs du formulaire */
+        String email = getValeurChamp( request, CHAMP_EMAIL );
+        String motDePasse = getValeurChamp( request, CHAMP_PASS );
+
+        Users utilisateur = new Users();
+
+        /* Validation du champ email. */
+        try {
+            validationEmail( email );
+        } catch ( Exception e ) {
+            setErreur( CHAMP_EMAIL, e.getMessage() );
+        }
+        utilisateur.setLogin( email );
+
+        /* Validation du champ mot de passe. */
+        try {
+            validationMotDePasse( motDePasse );
+        } catch ( Exception e ) {
+            setErreur( CHAMP_PASS, e.getMessage() );
+        }
+        utilisateur.setPassword( motDePasse );
+
+        /* Initialisation du résultat global de la validation. */
+        if ( erreurs.isEmpty() ) {
+            resultat = "Succès de la connexion.";
+        } else {
+            resultat = "Échec de la connexion.";
+        }
+
+        return utilisateur;
+    }
+
+    /**
+     * Valide l'adresse email saisie.
+     */
+    private void validationEmail( String email ) throws Exception {
+        if ( email != null && !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) {
+            throw new Exception( "Merci de saisir une adresse mail valide." );
+        }
+    }
+
+    /**
+     * Valide le mot de passe saisi.
+     */
+    private void validationMotDePasse( String motDePasse ) throws Exception {
+        if ( motDePasse != null ) {
+            if ( motDePasse.length() < 3 ) {
+                throw new Exception( "Le mot de passe doit contenir au moins 3 caractères." );
+            } else {
+                if(!motDePasse.equals(usersManager.find("password", motDePasse))){
+                    throw new Exception( "Le mot de passe est incorrect." );
+                }
+            }
+        } else {
+            throw new Exception( "Merci de saisir votre mot de passe." );
+        }
+    }
+
+    /*
+     * Ajoute un message correspondant au champ spécifié à la map des erreurs.
+     */
+    private void setErreur( String champ, String message ) {
+        erreurs.put( champ, message );
+    }
+
+    /*
+     * Méthode utilitaire qui retourne null si un champ est vide, et son contenu
+     * sinon.
+     */
+    private static String getValeurChamp( HttpServletRequest request, String nomChamp ) {
+        String valeur = request.getParameter( nomChamp );
+        if ( valeur == null || valeur.trim().length() == 0 ) {
+            return null;
+        } else {
+            return valeur;
+        }
+    }
+}
